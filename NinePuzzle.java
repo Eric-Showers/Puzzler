eae441c5299fbb39896c17cefcfdbfbe104bfa70
E. Showers <E. Showers>
2017-08-03 17:23:53 -0700

    updating source for appearances

--- NinePuzzle.java
+++ NinePuzzle.java
@@ -1,18 +1,16 @@
-/* NinePuzzle.java
-   CSC 225 - Spring 2017
-   Assignment 4 - Template for the 9-puzzle
+/* NinePuzzle.java Created by Eric Showers for CSC 225 - Spring 2017
+	 From a template created by:
+	 	B. Bird    - 07/11/2014
+		M. Simpson - 11/07/2015
 
    This template includes some testing code to help verify the implementation.
    Input boards can be provided with standard input or read from a file.
 
-   To provide test inputs with standard input, run the program with
-	java NinePuzzle
-   To terminate the input, use Ctrl-D (which signals EOF).
+   To provide test inputs with standard input, run the program with: java NinePuzzle
+   To provide test inputs from a file, run the program with:
+    java NinePuzzle [filename].txt
 
-   To read test inputs from a file (e.g. boards.txt), run the program with
-    java NinePuzzle boards.txt
-
-   The input format for both input methods is the same. Input consists
+   The data format for both input methods is the same. Input data consists
    of a series of 9-puzzle boards, with the '0' character representing the
    empty square. For example, a sample board with the middle square empty is
 
@@ -28,50 +26,44 @@
 
    An input file can contain an unlimited number of boards; each will be
    processed separately.
-
-   B. Bird    - 07/11/2014
-   M. Simpson - 11/07/2015
 */
 
 import java.util.*;
 import java.io.File;
 
 
-public class NinePuzzle{
+public class NinePuzzleFINAL{
 
 	//The total number of possible boards is 9! = 1*2*3*4*5*6*7*8*9 = 362880
 	public static final int NUM_BOARDS = 362880;
 
-	/*  SolveNinePuzzle(B)
-		Given a valid 9-puzzle board (with the empty space represented by the
-		value 0),return true if the board is solvable and false otherwise.
-		If the board is solvable, a sequence of moves which solves the board
-		will be printed, using the printBoard function below.
+	/*  SolveNinePuzzle(int[][]B)
+		Given a valid 9-puzzle board, return true if the board is solvable and false
+		otherwise. If the board is solvable, a sequence of moves which solves the
+		board will be printed, using the printBoard function below.
 	*/
 	public static boolean SolveNinePuzzle(int[][] B){
 
-		/* Boards may only be adjacent to AT MOST 4 other boards. The indices
-		in adjList correspond to:
+		/* Boards may only be adjacent to AT MOST 4 other boards. Indices [i][0-3]
+		in adjList correspond to the adjacent board id's:
 
-		[Board i][ZDOWN, ZUP, ZRIGHT, ZLEFT, Visited, Parent]
+		[Board i][ZeroDOWN, ZeroUP, ZeroRIGHT, ZeroLEFT, VisitedFlag, ParentBoardid]
 		[Board i+1]
 		[Board i+2]
 		.
 		.
 		.      */
-
-
 		int[][] adjList = new int[NUM_BOARDS][6];
 
 
-	/**************************************************************************************
-		Create the graph containing all possible game states (size is NUM_BOARDS)
-	**************************************************************************************/
+		/**************************************************************************
+			Create the graph containing all possible game states (size is NUM_BOARDS)
+		***************************************************************************/
 
-		/*Iterates once for every possible board. Finds the possible moves from
-			that board, then stores those board id's in the appropriate adjList
-			tag indice. If a move is not possible, that indice in adjList is set
-			to -1.*/
+		/* Iterates once for every possible board. Finds the possible moves from
+			that board, then stores those resulting board id's in the appropriate
+			adjList tag indice. If a move is not possible, that indice in adjList is
+			set to -1.*/
 		for (int i=0; i<NUM_BOARDS; i++){
 			int[][] curBoard = getBoardFromIndex(i);
 
@@ -129,35 +121,36 @@ public class NinePuzzle{
 		************************************************************/
 
 		//Get the start and target indices
-		int startIndex = getIndexFromBoard(B);
-		int[][] endBoard = new int[][] {
+		int givenIndex = getIndexFromBoard(B);
+		int[][] solvedBoard = new int[][] {
 			{1,2,3},
 			{4,5,6},
 			{7,8,0}
 		};
-		int endIndex = getIndexFromBoard(endBoard);
+		int solvedIndex = getIndexFromBoard(solvedBoard);
 		boolean solvable;
 
 		//Call BFS method
-		solvable = BFSpath(adjList, endIndex, startIndex);
+		solvable = BFSpath(adjList, solvedIndex, givenIndex);
 		return solvable;
 	}
 	/*Does a BFS from start, tracking each nodes parent in the traversal. Once
 	 	target is found it iterates through the parents of every node from
-		target-start, printing at each node. */
+		target-start, printing at each iteration. */
 	public static boolean BFSpath(int[][] adjList, int start, int target){
 		LinkedList<Integer> q = new LinkedList<Integer>();
 		int w = start;
 		//Visit first node
 		adjList[w][4] = 1;
-		q.add(w);
+		q.add(w);				//BFS queue
 
+		//BFS iterator
 		while (q.size() !=0){
-			//If target node found print node, then parent, until node=start
+			//If target node found print node, then iterate to parent until node=start
 			if (w == target){
 				while (w != start){
 					printBoard(getBoardFromIndex(w));
-					w = adjList[w][5];
+					w = adjList[w][5];			//Parent node of w
 				}
 				printBoard(getBoardFromIndex(start));
 				return true;
@@ -203,12 +196,7 @@ public class NinePuzzle{
 		return newBoard;
 	}
 
-	/*  printBoard(B)
-		Print the given 9-puzzle board. The SolveNinePuzzle method above should
-		use this method when printing the sequence of moves which solves the input
-		board. If any other method is used (e.g. printing the board manually), the
-		submission may lose marks.
-	*/
+	//Print the given 9-puzzle board
 	public static void printBoard(int[][] B){
 		for (int i = 0; i < 3; i++){
 			for (int j = 0; j < 3; j++)
@@ -220,9 +208,8 @@ public class NinePuzzle{
 
 
 	/* Board/Index conversion functions
-	   These should be treated as black boxes (i.e. don't modify them, don't worry about
-	   understanding them). The conversion scheme used here is adapted from
-		 W. Myrvold and F. Ruskey, Ranking and Unranking Permutations in Linear Time,
+	     The conversion scheme used here is adapted from W. Myrvold and
+		 F. Ruskey, Ranking and Unranking Permutations in Linear Time,
 		 Information Processing Letters, 79 (2001) 281-284.
 	*/
 	public static int getIndexFromBoard(int[][] B){
@@ -268,22 +255,18 @@ public class NinePuzzle{
 
 
 	public static void main(String[] args){
-		/* Code to test your implementation */
-		/* You may modify this, but nothing in this function will be marked */
-
-
 		Scanner s;
 
 		if (args.length > 0){
 			//If a file argument was provided on the command line, read from the file
-			try{
+			try {
 				s = new Scanner(new File(args[0]));
 			} catch(java.io.FileNotFoundException e){
 				System.out.printf("Unable to open %s\n",args[0]);
 				return;
 			}
 			System.out.printf("Reading input values from %s.\n",args[0]);
-		}else{
+		} else {
 			//Otherwise, read from standard input
 			s = new Scanner(System.in);
 			System.out.printf("Reading input values from stdin.\n");
